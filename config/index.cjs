6e326a1bb57e0a8af557ba7c5e242b9b6249e05c
zhangjiawei-hub <2820213235@qq.com>
2025-05-23 17:12:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '5c879e1fe394d98bfc9032ec1b10be2c',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '上海',
+  CITY: '上海',
 
   USERS: [
     {
